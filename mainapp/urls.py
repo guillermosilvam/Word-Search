fa09be9df42276a4475490a5d2245ba3b6b31fe4
guillermosilvam <panahermoso10@gmail.com>
2024-12-10 18:33:47 -0400

    updated user interface and some bug fixes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('game/', views.game, name='game'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
+
+def game(request):
+    return render(request, 'game.html')
