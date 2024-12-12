3b31e8f8d0ce883dd473f38b407935d8428d558f
guillermosilvam <panahermoso10@gmail.com>
2024-12-12 19:36:39 -0400

    ShowHide function done

---
+++ mainapp/static/js/scripts.js
@@ -0,0 +1,15 @@
+function showHide() {
+    var container = document.getElementById("rules");
+
+    if (container.style.visibility == "hidden") {
+        container.style.visibility = "visible";
+    } else {
+        container.style.visibility = "hidden";
+    }
+    
+}
+
+//window.onload = function pop_up() {
+//    var container = document.getElementById("rules");
+//    container.style.visibility = "visible";
+//}
