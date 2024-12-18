440e5b91e45b580aa33c1aa474124cc0a586e41c
guillermosilvam <panahermoso10@gmail.com>
2024-12-18 14:38:46 -0400

    letter spacing done

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,8 +6,18 @@ def home(request):
 
 def game(request):
     letter = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+
+    words = [
+        'Apple', 'Banana', 'Cat', 'Dog', 'Elephant', 'Frog', 'Giraffe', 'Horse', 'Iguana', 'Jaguar'
+    ]    
+    
+    game = {
+        'letter' : letter,
+        'words' : words
+    }
 
     
-    
-    return render(request, 'game.html', {'letter': letter})
+
+    return render(request, 'game.html', game)
